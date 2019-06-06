4168ceb309c4df6eb02bd448de600f93e0a53923
emilw <emil@postback.se>
2019-06-06 11:06:33 +0200

    Refactor to simplify testing

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://mqtt')
 const Gpio = require('onoff').Gpio;
 
-function watering(fake){
+function turnOn(fake){
     if(fake){
-        console.log("Water is turned on");
-        setTimeout(function(){
-            console.log("Watering is turned of");
-        },5000)
-    } else{
+        console.log("FAKE - Water is turned on");
+    } else {
         const led = new Gpio(120, 'out');
-        led.write(1);
-        setTimeout(function(){
+        led.writeSync(1)
+        led.unexport();
+    }
+
+    /*
+    Safety timeout when to close down the watering if no one turns it off
+    setTimeout(function(){
             led.writeSync(0);
             led.unexport();
-        },5000)
+        },5000)*/
+}
+
+function turnOff(fake){
+    if(fake){
+        console.log("FAKE - Water is turned off");
+    } else {
+        const led = new Gpio(120, 'out');
+        led.writeSync(0);
+        led.unexport();
+    }
+}
+
+function watering(fake, state){
+    if(state == "0"){
+        turnOff(fake);
+    } else{
+        turnOn(fake);
     }
 }
 
@@ -30,7 +49,8 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
-  watering(1);
+
+  watering(1, message.toString());
   //client.end()
 })
 
